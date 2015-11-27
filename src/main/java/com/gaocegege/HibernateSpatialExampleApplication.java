6e046b2a843b2d945fbce58698292a3ee4b1d58c
Gaoce <gaocegege@hotmail.com>
2015-11-27 12:39:45 +0800

    begin to work

---
+++ src/main/java/com/gaocegege/HibernateSpatialExampleApplication.java
@@ -0,0 +1,12 @@
+package com.gaocegege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpatialExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateSpatialExampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gaocegege/HibernateSpatialExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gaocegege;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HibernateSpatialExampleApplication.class)
+public class HibernateSpatialExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
